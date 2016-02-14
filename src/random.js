b7574c2e059ef74a050b5c67cd6c9c2d17e42d2e
Vasuman R <vasumanar@gmail.com>
2016-02-14 18:34:35 +0530

    Random letters and seeded

--- src/random.js
+++ src/random.js
@@ -1,13 +1,28 @@
+const M = 8278626;
+const P = 1;
+
+let s = 0;
+
+function n() {
+  s = Math.sin(P + s) * M;
+  s -= ~~(s);
+  return s;
+}
+
 export function seed(val) {
-  // TODO: implement
+  s = val;
 }
 
 export function color() {
   return `rgba(${i(0, 255)}, ${i(0, 255)}, ${i(0, 255)}, 255)`;
 }
 
+export function letters(n = 1) {
+  return Array.from(new Array(n), () => String.fromCharCode(i(97, 122))).join('');
+}
+
 export function f(s = 0, e = 1) {
-  return s + Math.random() * (e - s);
+  return s + n() * (e - s);
 }
 
 export function i(s, e) {
@@ -15,5 +30,5 @@ export function i(s, e) {
 }
 
 export function bool(bias = 1, exp = 1) {
-  return Math.random() / Math.pow(bias, exp) < 0.5;
+  return n() / Math.pow(bias, exp) < 0.5;
 }
