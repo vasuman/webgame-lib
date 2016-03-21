22366b1610b16997f2e0850499473a81bd9b9fdb
Vasuman R <vasumanar@gmail.com>
2016-03-21 21:56:56 +0530

    Load JSON data files

--- src/assets.js
+++ src/assets.js
@@ -1,10 +1,31 @@
 function loadImage(src) {
   let img = new Image();
   img.src = src;
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     img.addEventListener('load', () => {
       resolve(img);
     });
+    img.addEventListener('error', () => {
+      reject(new Error(`Failed to load image ${src}`));
+    });
+  });
+}
+
+function loadData(src) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', src, true);
+  xhr.send();
+  return new Promise((resolve, reject) => {
+    xhr.onlaod = () => {
+      try {
+        resolve(JSON.parse(xhr.responseText));
+      } catch (e) {
+        reject(new Error(`Failed to parse ${src} because ${e}`));
+      }
+    }
+    xhr.onerror = () => {
+      reject(new Error(`Failed to load data from ${src}`));
+    }
   });
 }
 
@@ -28,6 +49,11 @@ export default class Assets {
      * @type {Map<string, CanvasImageSource>}
      */
     this.images = new Map();
+    /**
+     * Maps JSON paths to data.
+     * @type {Map<string, Object>}
+     */
+    this.data = new Map();
   }
 
   /**
@@ -46,39 +72,40 @@ export default class Assets {
    *         scale: 1
    *       }
    *     }
-   *   }
+   *   },
+   *   data: ['data/level.json']
    * }
    */
   load(manifest) {
     let ps = [];
-
-    // load images
-    let srcs = Object.keys(manifest.images);
-    for (let src of srcs) {
-
-      let p = loadImage(this.prefix + src).then((img) => {
-        let defs = manifest.images[src];
-        let names = Object.keys(defs);
-
-        for (let name of names) {
-          let def = defs[name];
-          let can = document.createElement('canvas');
-          let scale = def.scale || 1;
-          let w = can.width = scale * def.w;
-          let h = can.height = scale * def.h;
-          let ctx = can.getContext('2d');
-          ctx.imageSmoothingEnabled = this._smoothImage;
-          ctx.drawImage(img, def.x, def.y, def.w, def.h, 0, 0, w, h);
-          this.images.set(name, can);
-        }
-
-      });
-
-      ps.push(p);
+    if (manifest.images) {
+      let srcs = Object.keys(manifest.images);
+      for (let src of srcs) {
+        let path = this.prefix + src;
+        ps.push(loadImage().then(img => {
+          let defs = manifest.images[src];
+          let names = Object.keys(defs);
+          for (let name of names) {
+            let def = defs[name];
+            let can = document.createElement('canvas');
+            let scale = def.scale || 1;
+            let w = can.width = scale * def.w;
+            let h = can.height = scale * def.h;
+            let ctx = can.getContext('2d');
+            ctx.imageSmoothingEnabled = this._smoothImage;
+            ctx.drawImage(img, def.x, def.y, def.w, def.h, 0, 0, w, h);
+            this.images.set(name, can);
+          }
+        }));
+      }
     }
-
+    if (manifest.data) {
+      let srcs = Object.keys(manifest.data);
+      for (let src of srcs) {
+        let path = this.prefix + src;
+        ps.push(loadData(path).then(data => this.data.set(path, data)));
+      }
+    }
     return Promise.all(ps);
   }
-
 }
-
